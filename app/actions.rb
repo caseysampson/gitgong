e5a934850630d1d3f3361f02ecf58214536fa869
caseysampson <caseysampson@hotmail.com>
2014-11-23 03:44:23 +0000

    index - repos

--- app/actions.rb
+++ app/actions.rb
@@ -2,15 +2,15 @@
 
 helpers do
   def take_repos
-    github = Github.new user: 'caseysampson', repo: 'music_wall'
-    github2 = Github.new user: 'jandlint', repo: 'git_gong'
+    github_repo_1 = Github.new user: 'caseysampson', repo: 'music_wall'
+    github_repo_2 = Github.new user: 'caseysampson', repo: 'contact_list_ar'
+    github_repo_3 = Github.new user: 'caseysampson', repo: 'two_player_math_game'
+    github_repo_4 = Github.new user: 'jandlint', repo: 'git_gong'
     @repos = []
-    @repos << github.issues.milestones.all 
-    @repos << github2.issues.milestones.all 
-  end
-
-  def find_elements
-    
+    @repos << github_repo_1.issues.milestones.all 
+    @repos << github_repo_2.issues.milestones.all 
+    @repos << github_repo_3.issues.milestones.all 
+    @repos << github_repo_4.issues.milestones.all 
   end
   
 end
