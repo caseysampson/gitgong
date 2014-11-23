591de7acb35a22d0817e46d055f027d0d3fa8aa5
jandlint <jandlint@gmail.com>
2014-11-23 21:41:02 +0000

    finish poller

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,13 +1,13 @@
 $(function () {
   var github_token = $('body').data('github-token');
   $.get( "https://api.github.com/repos/jandlint/git_gong/milestones?access_token="+github_token, function( data ) {
-    console.log( "Open Issues: " + data[0]['open_issues']);
-    console.log( "Close Issues: " + data[0]['closed_issues']);
-    console.log( "Updated Time: " + moment(data[0]['updated_at']).format());
-    console.log( "Current Time: " + moment().format());
+    window.first_repo_title = data[0]['title'];
+    // console.log( "Open Issues: " + data[0]['open_issues']);
+    // console.log( "Close Issues: " + data[0]['closed_issues']);
+    // console.log( "Repo Name: " + data[0]['title']);
+    //console.log( "Updated Time: " + moment(data[0]['updated_at']).format());
+    //console.log( "Current Time: " + moment().format());
   })
-  $.cookie('update_log', moment().format());
+  //$.cookie('update_log', moment().format());
+  $("p.thing").append(window.first_repo_title);
 });
-
-
-
