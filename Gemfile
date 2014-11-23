5c9e06d00bab9232eea99dd1eeb85f4531cdd0b2
jandlint <jandlint@gmail.com>
2014-11-23 07:31:43 +0000

    implement js

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'github_api'
 
 group :development, :test do
   gem 'pry'
+  gem 'better_errors'
   gem 'shotgun'
   gem 'sqlite3'
 end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,24 @@
-$(document).ready(function() {
+$(function () {
+  var sin = [], cos = [];
+  for (var i = 0; i < 14; i += 0.5) {
+    sin.push([i, Math.sin(i)])
+  }
+  var plot = $.plot($("#placeholder"),
+        [ { data: sin }, { data: cos } ], {
+          series: {
+            lines: { show: true },
+            points: { show: true },
+            color: '#b0007c'
+          },
+          grid: { hoverable: true, clickable: true },
+          yaxis: { min: -1.2, max: 1.2 }
+        });
+  $("#placeholder").bind("plotclick", function (event, pos, item) {
+      if (item) {
+        $("#clickdata").text("You clicked point " + item.dataIndex + " in " + item.series.label + ".");
+        plot.highlight(item.series, item.datapoint);
+      }
+  });
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  setInterval(function () {alert("Hello")}, 3000);
 });
