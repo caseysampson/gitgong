32c19d9df98e9835158298a7ba561c8531168bce
jandlint <jandlint@gmail.com>
2014-11-23 18:02:22 +0000

    Add first dummy repo

--- app/actions.rb
+++ app/actions.rb
@@ -6,7 +6,7 @@ helpers do
     @repos = []
     @repos << github.issues.milestones.all(user: 'caseysampson', repo: 'music_wall')
     @repos << github.issues.milestones.all(user: 'caseysampson', repo: 'contact_list_ar')
-    @repos << github.issues.milestones.all(user: 'caseysampson', repo: 'two_player_math_game')
+    @repos << github.issues.milestones.all(user: 'tinderforcats', repo: 'tinder_integration_api')
     @repos << github.issues.milestones.all(user: 'jandlint', repo: 'git_gong')
   end
   
