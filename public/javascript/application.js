47dde9062b35bc4349afb760b7301b54339579ae
jandlint <jandlint@gmail.com>
2014-11-24 02:53:34 +0000

    add loop for JSON extraction

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,19 +1,29 @@
-var firstrepo = {};
+var repositories = [
+  "https://api.github.com/repos/tinderforcats/tinder_integration_api/milestones?access_token=",
+  "https://api.github.com/repos/tinderforcats/mobile_api/milestones?access_token=",
+  "https://api.github.com/repos/tinderforcats/client/milestones?access_token=",
+  "https://api.github.com/repos/tinderforcats/REST_api/milestones?access_token="
+]
+
+var milehash = {};
+
+var render()
 
 $(function () {
   var github_token = $('body').data('github-token');
-  $.get( "https://api.github.com/repos/tinderforcats/tinder_integration_api/milestones?access_token="+github_token, function( data ) {
-    var take = Iterator(data);
-      for (var milestone in take)
-      firstrepo["title"] = milestone["title"];
-      firstrepo["closed_issues"] = milestone["closed_issues"];
-      firstrepo["open_issues"] = milestone["open_issues"];
-    var check = Iterator(firstrepo)
-      for (var title in check)
-      console.log("title");
-    // var percentage = data.closed_issues / data.open_issues;
-  })
-  //$.cookie('update_log', moment().format());
+  var load = setInterval(
+    function(){
+      for (url in repositories) {
+        $.get( url + github_token, function( data ) {
+            for (var i in data) {
+              milehash[data[i].id] = {
+                title: data[i].title,
+                percentage: 100 * (data[i].open_issues / (data[i].open_issues + data[i].closed_issues))
+              };
+            }
+          console.log(milehash);
+        });
+      }
+      // rendering function
+    }, 5000) 
 });
-
-// setInterval
