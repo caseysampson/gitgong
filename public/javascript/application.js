0106054d696ff627964bfb54b77105e0494f7a90
jandlint <jandlint@gmail.com>
2014-11-24 01:14:36 +0000

    start javascript again

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,13 +1,19 @@
+var firstrepo = {};
+
 $(function () {
   var github_token = $('body').data('github-token');
-  $.get( "https://api.github.com/repos/jandlint/git_gong/milestones?access_token="+github_token, function( data ) {
-    window.first_repo_title = data[0]['title'];
-    // console.log( "Open Issues: " + data[0]['open_issues']);
-    // console.log( "Close Issues: " + data[0]['closed_issues']);
-    // console.log( "Repo Name: " + data[0]['title']);
-    //console.log( "Updated Time: " + moment(data[0]['updated_at']).format());
-    //console.log( "Current Time: " + moment().format());
+  $.get( "https://api.github.com/repos/tinderforcats/tinder_integration_api/milestones?access_token="+github_token, function( data ) {
+    var take = Iterator(data);
+      for (var milestone in take)
+      firstrepo["title"] = milestone["title"];
+      firstrepo["closed_issues"] = milestone["closed_issues"];
+      firstrepo["open_issues"] = milestone["open_issues"];
+    var check = Iterator(firstrepo)
+      for (var title in check)
+      console.log("title");
+    // var percentage = data.closed_issues / data.open_issues;
   })
   //$.cookie('update_log', moment().format());
-  $("p.thing").append(window.first_repo_title);
 });
+
+// setInterval
