592eafeb5078292dc4ebb27fe4f112dd9369a8b2
jandlint <jandlint@gmail.com>
2014-11-24 03:45:15 +0000

    stub render method

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -5,9 +5,45 @@ var repositories = [
   "https://api.github.com/repos/tinderforcats/REST_api/milestones?access_token="
 ]
 
-var milehash = {};
+var mileobject = {
+  /*
 
-var render()
+This is what the object looks like:
+
+  milestoneid: {
+    title: title
+    percentage: x%
+  }
+  */
+};
+
+// var o = {
+//   'x': {
+//     title: 'hello',
+//     percentage: 5
+//   },
+//   'y': {
+//     title: "hi",
+//     percentage: 10
+//   }
+// }
+// var myvar = 'x';
+// myvar.title;
+
+var render = function(object) {
+  for (milestoneid in object){
+    // clarify this line below, you're confused
+    var value = object[milestoneid];
+    $('h#' + milestoneid).css({});
+    $('div#' + milestoneid).css({"width": milestoneid.percentage + "%"});
+    if (milestoneid.percentage == 100) {
+      // crazy shit
+    }
+    else {
+      // not so crazy shit
+    }
+  }
+}
 
 $(function () {
   var github_token = $('body').data('github-token');
@@ -16,14 +52,14 @@ $(function () {
       for (url in repositories) {
         $.get( url + github_token, function( data ) {
             for (var i in data) {
-              milehash[data[i].id] = {
+              mileobject[data[i].id] = {
                 title: data[i].title,
                 percentage: 100 * (data[i].open_issues / (data[i].open_issues + data[i].closed_issues))
               };
             }
-          console.log(milehash);
+          console.log(mileobject);
         });
       }
-      // rendering function
+      render(mileobject)
     }, 5000) 
 });
